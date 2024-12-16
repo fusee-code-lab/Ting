abf5ca4aef6e3ae55faf6dd2f9a72e8248ce3038
youliso <youliso@hotmail.com>
2024-12-16 22:56:36 +0800

    update

--- scripts/buildBeforePack.js
+++ scripts/buildBeforePack.js
@@ -2,11 +2,9 @@ const bytecode = require('./plugins/bytecode');
 
 exports.default = async ({ packager }) => {
   try {
-    if (!packager.info._configuration.asar) {
-      //v8字节码  如果在windows64下打包32位请先将electron 切换到32位
-      let arch = packager.appInfo.platformSpecificOptions.target[0].arch[0];
-      await bytecode(arch);
-    }
+    //v8字节码  如果在windows64下打包32位请先将electron 切换到32位
+    let arch = packager.appInfo.platformSpecificOptions.target[0].arch[0];
+    await bytecode(arch);
   } catch (err) {
     console.error(err);
   }
