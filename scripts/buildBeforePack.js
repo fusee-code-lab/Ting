d58760a57433a4caa140044a903b202262ec1e89
youli <youliso@hotmail.com>
2025-01-16 10:35:11 +0800

    refactor(build): 重构构建流程并支持多平台打包
    - 修改 buildAfterPack.js, buildAfterSign.js 和 buildBeforePack.js 以支持不同平台的打包需求
    - 更新 bytecode 和 fuses 插件以适应多平台环境
    - 调整 release.cjs 脚本，实现自动检测平台并进行相应构建

--- scripts/buildBeforePack.js
+++ scripts/buildBeforePack.js
@@ -1,10 +1,9 @@
 const bytecode = require('./plugins/bytecode');
 
-exports.default = async ({ packager }) => {
+exports.default = async ({ electronPlatformName, arch }) => {
   try {
     //v8字节码  如果在windows64下打包32位请先将electron 切换到32位
-    let arch = packager.appInfo.platformSpecificOptions.target[0].arch[0];
-    await bytecode(arch);
+    await bytecode(electronPlatformName, arch);
   } catch (err) {
     console.error(err);
   }
